4cc76fffc5838484cccadf2843cd200573836edf
orm <rimoll@gmail.com>
2011-03-19 20:44:08 -0400

    lab 5 part 1 implemented but not tested. have problems in ./star.sh

--- extent_client.cc
+++ extent_client.cc
@@ -12,6 +12,7 @@
 extent_client::extent_client(std::string dst)
 {
   sockaddr_in dstsock;
+  pthread_mutex_init(&cache_table_mutex, NULL);
   make_sockaddr(dst.c_str(), &dstsock);
   cl = new rpcc(dstsock);
   if (cl->bind() != 0) {
@@ -22,8 +23,19 @@ extent_client::extent_client(std::string dst)
 extent_protocol::status
 extent_client::get(extent_protocol::extentid_t eid, std::string &buf)
 {
+
   extent_protocol::status ret = extent_protocol::OK;
-  ret = cl->call(extent_protocol::get, eid, buf);
+  
+  ScopedLock sl(&cache_table_mutex);  
+  cache_row_t & row = cache_table[eid];
+
+  if (row.current){
+    buf = row.buf;
+  } else {
+    ret = cl->call(extent_protocol::get, eid, buf);
+    row.current = true;
+    row.buf = buf;
+  }
   return ret;
 }
 
@@ -40,8 +52,15 @@ extent_protocol::status
 extent_client::put(extent_protocol::extentid_t eid, std::string buf)
 {
   extent_protocol::status ret = extent_protocol::OK;
-  int r;
-  ret = cl->call(extent_protocol::put, eid, buf, r);
+  //  int r;
+  cache_row_t & row = cache_table[eid];
+  assert(row.current);
+  assert(buf != row.buf);
+  row.buf = buf;
+  row.dirty = true;
+  
+  //  ret = cl->call(extent_protocol::put, eid, buf, r);
+
   return ret;
 }
 
@@ -49,9 +68,9 @@ extent_protocol::status
 extent_client::remove(extent_protocol::extentid_t eid)
 {
   extent_protocol::status ret = extent_protocol::OK;
-  int r;
-  ret = cl->call(extent_protocol::remove, eid, r);
+  //  int r;
+  
+  cache_table.erase(eid);
+  //  ret = cl->call(extent_protocol::remove, eid, r);
   return ret;
 }
-
-
--- extent_client.h
+++ extent_client.h
@@ -7,10 +7,22 @@
 #include "extent_protocol.h"
 #include "rpc.h"
 
+using namespace std;
 class extent_client {
+  
+  typedef struct cache_row {
+    bool dirty;
+    bool current;
+    string buf;
+    
+  cache_row(): dirty(false), current(false), buf("INVALID"){}
+  } cache_row_t;
+
  private:
   rpcc *cl;
-
+  map<extent_protocol::extentid_t, cache_row_t> cache_table;
+  pthread_mutex_t cache_table_mutex;
+   
  public:
   extent_client(std::string dst);
 
